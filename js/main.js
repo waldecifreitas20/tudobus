da338fe1b2bed950b1a334d43884adf5ac5537f7
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-01-29 11:51:11 -0300

    feat: toggler menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const menuToggler = document.getElementById("btn-menu");
+const navMenu = document.querySelector("header .menu");
+
+menuToggler.addEventListener('change', evt => {
+    const openMenu = evt.target.checked;
+    
+    if (openMenu) {
+        navMenu.classList.remove('dispose');        
+    } else {
+        navMenu.classList.add('dispose');        
+    }
+});
