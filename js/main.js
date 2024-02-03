cfa93654c4f56deeb3151508e6ee76912e767c8b
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-02-03 15:23:34 -0300

    feat: medium and large responsible menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
-const menuToggler = document.getElementById("btn-menu");
+const menuToggler = document.getElementById("menu-toggler");
 const navMenu = document.querySelector("header .nav-menu");
 
-menuToggler.addEventListener('change', evt => {
-    const openMenu = evt.target.checked;
-    
-    if (openMenu) {
-        navMenu.classList.remove('dispose');        
-    } else {
-        navMenu.classList.add('dispose');        
-    }
+let isMenuHidden = true;
+let maxWindowWidth = 768;
+
+window.addEventListener('load', () => {
+    toggleMenu(window.innerWidth <= maxWindowWidth);
 });
+
+/* dispose menu if window is extra small after resizing */
+window.addEventListener('resize', () => {
+    toggleMenu(window.innerWidth <= maxWindowWidth);
+
+});
+
+/* toggle menu display */
+menuToggler.addEventListener('change', () => {
+    isMenuHidden = !isMenuHidden;    
+    toggleMenu(isMenuHidden);
+});
+
+
+/* Open and dispose menu  */
+const toggleMenu = (isHidden) => {
+
+    isMenuHidden = isHidden;
+    if (isHidden) {
+        disposeMenu();
+    } else {
+        showMenu();
+    }
+}
+
+
+const disposeMenu = () => navMenu.classList.add('dispose');
+const showMenu = () => navMenu.classList.remove('dispose');
+
